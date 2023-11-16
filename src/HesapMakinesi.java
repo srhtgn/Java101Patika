b687e9aed80b6b98e35c2fc9811e1ce52d2737b6
srhtgn <71978942+srhtgn@users.noreply.github.com>
2023-11-16 18:41:06 +0300

    Hesap Makinesi

---
+++ src/HesapMakinesi.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class HesapMakinesi {
+
+    public static void main(String[] args) {
+
+        int n1, n2, select;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = input.nextInt();
+
+        System.out.print("İkinci Sayıyı giriniz: ");
+        n2 = input.nextInt();
+
+        System.out.println("1- Toplama\n2- Çıkarma\n3- Çarpma\n4- Bölme");
+        System.out.print("Seçiminiz: ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Toplam : " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Çıkarma : " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Çarpma : " + (n1 * n2));
+                break;
+            case 4:
+                System.out.println("Bölme : " + ((n2 != 0) ? (n1 / n2) : "Bölen sayı sıfır olamaz!"));
+                break;
+            default:
+                System.out.println("Geçersiz seçim! Tekrar deneyiniz.");
+        }
+    }
+}
