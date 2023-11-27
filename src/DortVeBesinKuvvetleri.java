8bef8b1db78d54bd8eb3ec87e68b8483a03d9641
srhtgn <71978942+srhtgn@users.noreply.github.com>
2023-11-27 23:13:25 +0300

    Dort Ve Besin Kuvvetleri

---
+++ src/DortVeBesinKuvvetleri.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DortVeBesinKuvvetleri {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+
+        System.out.println("4 ve 5'in kuvvetleri:");
+
+        for (int i = 1; i <= sayi; i++) {
+            System.out.println("4^" + i + " = " + Math.pow(4, i));
+            System.out.println("5^" + i + " = " + Math.pow(5, i));
+        }
+
+        scanner.close();
+    }
+}
